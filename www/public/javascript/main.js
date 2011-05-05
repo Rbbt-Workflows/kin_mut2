af2af245568c20aaf60109c1502168ecb3b6f7b8
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2011-05-05 18:13:13 +0200

    Detail tabs

--- www/public/javascript/main.js
+++ www/public/javascript/main.js
@@ -3,3 +3,37 @@ function show_hide(){
   target.toggle();
   return false;
 }
+
+function tabs(elem){
+ var tabs = elem.find('li > a')
+ tabs.click(select_tab)
+ 
+ var active = elem.find('li > a.active').first();
+
+ if (active == null)
+  tabs.first().click();
+ else
+  active.click();
+
+}
+
+function select_tab(){
+  var target = $(this).attr('target');
+  var tab_list = $(this).parent('li').parent('ul');
+
+  var other_tabs = tab_list.find('li > a');
+
+  other_tabs.each(function(key,value){
+    var other_target = $(value).attr('target');
+    $(value).removeClass('active')
+    $(other_target).hide()
+  })
+
+  $(target).show()
+  $(this).addClass('active')
+
+
+  return(false);
+}
+
+
